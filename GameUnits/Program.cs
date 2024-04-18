6570415015988c40eb764f8709729b8e0822fab8
Julia Costa <juliacosta232005@gmail.com>
2024-04-19 00:34:59 +0100

    Add new Program.cs

--- GameUnits/Program.cs
+++ GameUnits/Program.cs
@@ -2,22 +2,35 @@
 
 namespace GameUnits
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Unit setUn = new SettlerUnit();
-            Unit milUn = new MilitaryUnit(2,3,4);
-            Unit[] units = new Unit[2] {setUn, milUn};
+            // Criar array com três unidades
+            Unit[] units = new Unit[]
+            {
+                new MilitaryUnit(3, 10, 2),
+                new MilitaryUnit(4, 5, 3),
+                new SettlerUnit(),
+            };
 
+            // Unidade 0 ataca unidade 1
+            (units[0] as MilitaryUnit).Attack(units[1]);
+            // Unidade 0 ataca unidade 2
+            (units[0] as MilitaryUnit).Attack(units[2]);
+
+            // "Imprimir" cada unidade
+            // chamando implicitamente o método ToString() de cada uma delas
             foreach (Unit u in units)
             {
-                u.Move();
-                Console.WriteLine(u.Health);
-                Console.WriteLine(u.Cost);
-                Console.WriteLine(u.ToString());
+                Console.WriteLine(u);
             }
-            
+
+            // Output esperado:
+            //
+            // MilitaryUnit: HP=12 COST=4 AP=2 XP=2
+            // MilitaryUnit: HP=3 COST=3 AP=3 XP=0
+            // SettlerUnit: HP=1 COST=5
         }
     }
 }
