05ea36025d741a28adf8191be9a2f84d5977aaee
Julia Costa <juliacosta232005@gmail.com>
2024-04-19 13:25:30 +0100

    Add constructor to SpyUnit class

--- GameUnits/SpyUnit.cs
+++ GameUnits/SpyUnit.cs
@@ -2,8 +2,12 @@ using System;
 
 namespace GameUnits
 {
-    public class SpyUnit
+    public class SpyUnit : XPUnit
     {
+        public override float Cost { get; } = 12.5f;
+        public SpyUnit(int mov=8,int health=2) : base(mov,health)
+        {
+        }
         
     }
 }
