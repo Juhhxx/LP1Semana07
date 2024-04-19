6e1f0aebc10c0bb6578ce14d53a30ecc34db8299
Julia Costa <juliacosta232005@gmail.com>
2024-04-19 13:26:04 +0100

    Add GetSecretInfo() method to SpyUnit class

--- GameUnits/SpyUnit.cs
+++ GameUnits/SpyUnit.cs
@@ -8,6 +8,24 @@ namespace GameUnits
         public SpyUnit(int mov=8,int health=2) : base(mov,health)
         {
         }
-        
+        public void GetSecretInfo(Unit unit)
+        {
+            int increment;
+
+            if (unit is SpyUnit)
+            {
+                increment = 3;
+            }
+            else if (unit is MilitaryUnit)
+            {
+                increment = 2;
+            }
+            else
+            {
+                increment = 1;
+            }
+
+            XP += increment;
+        }
     }
 }
